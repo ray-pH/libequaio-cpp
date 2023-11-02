c697af83b907d17cca651025b4db31e56aa98308
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-11-02 21:13:07 +0700

    arithmetic: add `operation_name`

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -13,6 +13,13 @@ typedef enum{
     OPERATOR_DIV = '/',
 } Operator;
 
+const map<Operator, string> operation_name = {
+    {OPERATOR_ADD, "add"},
+    {OPERATOR_SUB, "subtract"},
+    {OPERATOR_MUL, "multiply"},
+    {OPERATOR_DIV, "divide"},
+};
+
 Expression create_calculation(int left, int right, Operator op);
 Expression create_calculation(float left, float right, Operator op);
 
