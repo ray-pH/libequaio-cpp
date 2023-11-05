742b22c4dee636ef98eca9ca94835c1bf7871df4
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-11-05 21:08:06 +0700

    test: add spec for the interface

---
+++ test/interface.spec.cpp
@@ -0,0 +1,23 @@
+#include "doctest.h"
+#include "libequaio.h"
+#include "interface.h"
+// #include "parser.h"
+// #include "arithmetic.h"
+
+#include <iostream>
+using namespace std;
+
+TEST_CASE("Interface"){
+
+    SUBCASE("Arithmetic"){
+        Context ctx = {
+            {"x"},
+            {"+", "-", "*", "/", "="},
+            {"_"},
+            true,
+        };
+        Task task = { ctx };
+        task.set_target_eq("x = 3");
+        task.set_current_eq("18 + (2 * x)  = 24");
+    }
+}
